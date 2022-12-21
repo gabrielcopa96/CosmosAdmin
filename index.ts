5c12ae2a0b9c1a674d9f941003ec470bb2360735
Gabriel <gabrielcncopa@gmail.com>
2022-12-21 17:49:44 -0300

    modified post invoices, and customer

--- index.ts
+++ index.ts
@@ -7,12 +7,13 @@ import { connection } from './src/config/db';
 // almaceno la variable de entorno del puerto 
 // y le digo que es un string para que luego
 // typescript no me tire error
-const PORT = process.env.PORT as string
+const PORT: string | undefined = process.env.PORT;
 
 // por medio de la conexion en caso de que se resuelva 
 // la conexion a la base de datos, muestro en consola
 // que tanto el servidor como la base de datos, estan conectadas
 connection.then(() => {
+    console.log('connection to database established')
     app.listen(PORT, () => {
         console.log(`Servidor listo en el puerto ${PORT}`)
     })
